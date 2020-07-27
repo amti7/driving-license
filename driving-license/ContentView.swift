ce35e9386e41be5c045b49d05a75c9036f4a3541
Kamil Gacek <kamil.gacek@atendesoftware.pl>
2020-07-27 14:08:01 +0200

    Initial Commit

---
+++ driving-license/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  driving-license
+//
+//  Created by Kamil on 27/07/2020.
+//  Copyright © 2020 K&K. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
